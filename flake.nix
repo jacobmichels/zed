af794dda08f6d0b8787e9b2675ab858071612f50
jacobmichels <jacob.michels2025@gmail.com>
2025-11-07 23:04:49 -0500

    remove devshell from flake - I don't want garnix building that with my free minutes

--- flake.nix
+++ flake.nix
@@ -42,11 +42,6 @@
         default = mkZed pkgs;
         debug = default.override { profile = "dev"; };
       });
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./nix/shell.nix {
-          zed-editor = packages.${pkgs.hostPlatform.system}.default;
-        };
-      });
       formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
       overlays.default = final: _: {
         zed-editor = mkZed final;
